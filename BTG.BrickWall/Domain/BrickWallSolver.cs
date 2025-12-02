df4686f2e181f7768ad610a199ec0983b76bf3fd
William Abreu <william.abreu@klir.com>
2025-12-02 17:10:11 -0300

    implementation

--- BTG.BrickWall/Domain/BrickWallSolver.cs
+++ BTG.BrickWall/Domain/BrickWallSolver.cs
@@ -2,5 +2,28 @@ namespace BTG.BrickWall.Domain;
 
 public class BrickWallSolver
 {
-    
+    public int LeastBricks(IList<IList<int>> wall)
+    {
+        Dictionary<int, int> edgeCount = new Dictionary<int, int>();
+        int maxEdgesAligned = 0;
+
+        foreach (var row in wall)
+        {
+            int position = 0;
+
+            for (int i = 0; i < row.Count - 1; i++)
+            {
+                position += row[i];
+
+                if (!edgeCount.ContainsKey(position))
+                    edgeCount[position] = 0;
+
+                edgeCount[position]++;
+
+                maxEdgesAligned = Math.Max(maxEdgesAligned, edgeCount[position]);
+            }
+        }
+
+        return wall.Count - maxEdgesAligned;
+    }
 }
--- BTG.BrickWall/Program.cs
+++ BTG.BrickWall/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using BTG.BrickWall.Domain;
+
+var wall = new List<IList<int>> 
+{
+    new List<int>{1,2,2,1},
+    new List<int>{3,1,2},
+    new List<int>{1,3,2},
+    new List<int>{2,4},
+    new List<int>{3,1,2},
+    new List<int>{1,3,1,1}
+};
+
+BrickWallSolver solver = new BrickWallSolver();
+int result = solver.LeastBricks(wall);
+
+Console.WriteLine(result);
